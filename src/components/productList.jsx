8f41de8f0a35016dcfeff0623e53fbf31ae696d6
hope664 <umuhozahoe5@gmail.com>
2026-04-09 00:54:39 +0200

    working on frontend

---
+++ src/components/productList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import axios from "axios";
+
+function ProductForm({ fetchProducts }){
+    xonst [FormData, setForm] = useState({
+        name:"",
+        price:"",
+        stock:""
+        });
+
+        
+}
+
